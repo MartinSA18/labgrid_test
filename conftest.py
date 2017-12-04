f94644cdd087fe185b7e6c954eecf31aa91d4724
MartinSA18 <martin.s.andersen@gmail.com>
2017-12-04 13:15:54 +0100

    Outcommented fixture that was used for testing

--- conftest.py
+++ conftest.py
@@ -8,9 +8,10 @@ def command(target):
     shell = target.get_driver(CommandProtocol)
     target.activate(shell)
     return shell
-    
+"""    
 @pytest.fixture(scope='session')
 def cmd_line(target):
     shell = target.get_driver(ConsoleProtocol)
     target.activate(shell)
     return shell
+    """
