39b1783c85d52f43295b4f73eb2bd06f87d09171
MartinSA18 <martin.s.andersen@gmail.com>
2017-12-20 14:34:57 +0100

    Updated gosh driver with read/write and get_param_state functions

--- goshdriver.py
+++ goshdriver.py
@@ -1,42 +1,80 @@
 import attr
+import fcntl
+import logging
+import os
+import select
+import shlex
+import subprocess
+import re
 
 from labgrid.factory import target_factory
 from labgrid.driver.common import Driver
+from labgrid.driver import SerialDriver
 from labgrid.driver.consoleexpectmixin import ConsoleExpectMixin
 from labgrid.protocol import ConsoleProtocol, CommandProtocol
 from labgrid.resource import SerialPort
-from labgrid.util import Timeout, gen_marker
+from labgrid.util import Timeout, gen_marker, PtxExpect
 from labgrid.step import step
 
 @target_factory.reg_driver
 @attr.s(cmp=False)
-class GoshDriver(Driver, CommandProtocol):
-
-    cmd = attr.ib(validator=attr.validators.instance_of(str))
-    txdelay = attr.ib(default=0.0, validator=attr.validators.instance_of(float))
-
+class GoshDriver(ConsoleExpectMixin, Driver, CommandProtocol, ConsoleProtocol):
 
     bindings = {"console": ConsoleProtocol}
     
 
     def __attr_post_init__(self):
-        super().__attr_post_init__()
-
+        super().__attrs_post_init__()
+        
         
     @Driver.check_active
     @step(args=['cmd'], result=True)
     def run(self, cmd, *, step):
-        self.sendline(cmd)
-        
-        
+        self.console.sendline(cmd)
+        return
+       
+       
     @Driver.check_active
-    def run_check(self, cmd):
-        return 
-
-    @step()
-    def get_status(self):
+    @step(args=['cmd'], result=True)
+    def run_check(self, cmd, *, step):
+        self.console.sendline(cmd)
         return
         
+            
+        
+    @Driver.check_active
+    @step(args=['param'], result=True)
+    def get_param_state(self, param, expstate: int, *, step):
+    
+        command = str("param get " + param)
+        matchtrue = param + " = " + str(expstate)
+        self.console.sendline(command)
+        index, before, match, after = self.console.expect(bytes(matchtrue, 'utf-8'), timeout=1)
+        
+        result_str = ''.join(str(re.compile(str(match))))
+        
+        if result_str != "":
+            return True
+        else:
+            return False  
+        
+    @step()
+    def get_status(self):
+        self.console.sendline('')
+        index, before, match, after = self.console.expect(bytes(' #', 'utf-8'), timeout=1)
+        
+        result_str = ''.join(str(re.compile(str(index))))
+        
+        if result_str != "":
+            return 1
+        else:
+            return 0
+        
+        return index
+        
+      
+      
+      
         
     @Driver.check_active
     @step(args=['cmd', 'pattern'])
--- test_shell.py
+++ test_shell.py
@@ -1,3 +1,20 @@
 
 def test_gosh(command):
-    print(command.run('cmp ident'))
+    #command.run('param list 0')
+    #print("Hello World!")
+    #print(command.run_check('param list 0'))
+    #print(command.check_ram_image('param list 0'))
+    #command.run('param get ram_image')
+    
+    print("State = " + str(command.get_status()))
+    
+    if command.get_param_state('ram_image', 1):
+        print("Succes!")
+        
+    else:
+        print("Failure..")
+    
+    
+    #print(string_read)
+    
+    
